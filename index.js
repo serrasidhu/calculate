3c6e39510e292d1ba6ecf68a51658d360d62a9e3
Sara Murtaza <140093107+serrasidhu@users.noreply.github.com>
2023-09-27 12:26:29 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+import inquirer from 'inquirer';
+const result = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numOne",
+        message: "enter num 1"
+    },
+    {
+        type: "number",
+        name: "numTwo",
+        message: "enter num 2 "
+    },
+    {
+        type: 'list',
+        name: "operators",
+        choices: ["+", "-", "/", "*", "**", "%"],
+        message: "select operators : "
+    }
+]);
+let answer = 0;
+const { numOne, numTwo, operators } = result;
+if (numOne && numTwo && operators) {
+    if (operators === "+") {
+        answer = numOne + numTwo;
+    }
+    else if (operators === "-") {
+        answer = numOne - numTwo;
+    }
+    else if (operators === "/") {
+        answer = numOne / numTwo;
+    }
+    else if (operators === "*") {
+        answer = numOne * numTwo;
+    }
+    else if (operators === "**") {
+        answer = numOne ** numTwo;
+    }
+    else if (operators === "%") {
+        answer = numOne % numTwo;
+    }
+}
+else {
+    console.log("error");
+}
+console.log('the answer is:', answer);
